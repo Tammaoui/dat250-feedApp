22a0b0a6cff88369da3921ccb134e58315bfd546
Ayoub Tammaoui <ayoub.tammaoui@knowit.no>
2021-09-13 15:12:43 +0200

    Created Poll entity

---
+++ eclipselink/jpa-basic/src/main/java/entities/Poll.java
@@ -0,0 +1,82 @@
+package entities;
+
+import javax.persistence.*;
+
+
+import java.sql.Date;
+
+@Entity
+public class Poll {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private boolean active;
+
+    private boolean isPublic;
+
+    @ManyToOne
+    private User createdBy;
+
+    private Date lastEdited;
+
+    private Date createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public boolean isPublic() {
+        return isPublic;
+    }
+
+    public void setPublic(boolean aPublic) {
+        isPublic = aPublic;
+    }
+
+    public User getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(User createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getLastEdited() {
+        return lastEdited;
+    }
+
+    public void setLastEdited(Date lastEdited) {
+        this.lastEdited = lastEdited;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
